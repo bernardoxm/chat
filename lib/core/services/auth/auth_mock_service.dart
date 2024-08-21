7152eac25190885d3b85d1dfa1811aeff8cbc582
ber <bernardomelofacu@gmail.com>
2024-08-21 01:02:10 -0300

    comit

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -4,29 +4,28 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  @override
-  // TODO: implement currentUser
-  ChatUser? get currentUser => throw UnimplementedError();
+ static Map<String,ChatUser> _users = {};
+static ChatUser? _currentUser;
 
   @override
-  Future<void> login(String email, String password) {
-    // TODO: implement login
+  ChatUser? get currentUser {return null;}
+
+  @override
+  Future<void> login(String email, String password) async {
     throw UnimplementedError();
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
+  Future<void> logout() async {
     throw UnimplementedError();
   }
 
   @override
-  Future<void> signup(String name, String email, String password, File image) {
-    // TODO: implement signup
+  Future<void> signup(
+      String name, String email, String password, File image) async {
     throw UnimplementedError();
   }
 
   @override
-  // TODO: implement userChanges
-  Stream<ChatUser?> get userChanges => throw UnimplementedError();
+  Stream<ChatUser?> get userChanges {throw UnimplementedError();}
 }
